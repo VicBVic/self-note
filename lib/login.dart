b2d480c67a11980bfc48a6104f03c80a660c64d0
Acatrinei Robert-Marian <r.acatrieni2005@gmail.com>
2022-05-27 20:47:23 +0300

    some changes

---
+++ lib/login.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
