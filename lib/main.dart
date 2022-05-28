a57931dcf6f422b5037e9779c071ddd5ee6b1b60
RobBobBot <r.acatrinei2005@gmail.com>
2022-05-28 19:40:36 +0300

    sign in

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/paperbad',
+      initialRoute: '/papers',
       routes: {
         '/papers': (context) => MyHomePage(
               title: 'Bad thoughts paper',
