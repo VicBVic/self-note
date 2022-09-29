79f8ae2a1549ade2bfc30e76ff153d1a44334396
VicBVic <0.BotnaruVictor.0@gmail.com>
2022-09-29 20:56:46 +0300

    singuru care mere

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:itec20222/widgets/cookies.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SigninPage extends StatefulWidget {
@@ -11,53 +12,56 @@ class SigninPage extends StatefulWidget {
   State<SigninPage> createState() => _SigninPageState();
 }
 
+Future<bool> signin(String email, String pass, var context) async {
+  print("signin here $email $pass");
+
+  String error = "";
+
+  try {
+    final credential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: pass);
+
+    var user = FirebaseAuth.instance.currentUser;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('username', user!.uid);
+  } on FirebaseAuthException catch (e) {
+    error = e.code;
+  }
+
+  if (error != "") {
+    print('zyz: ' + error);
+    showDialog(
+        context: context,
+        builder: (ontext) => AlertDialog(
+              title: Text("Error"),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text(error),
+                    Text("Please try again!"),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  child: Text("Ok"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ));
+    return false;
+  }
+  var cookie = Cookies();
+  print("here bossulica");
+  cookie.setStoredLoginInfo([email, pass]);
+  return true;
+}
+
 class _SigninPageState extends State<SigninPage> {
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
-
-  Future<void> signin() async {
-    String email = emailcontroller.text;
-    String pass = passwordcontroller.text;
-    String error = "";
-
-    try {
-      final credential = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: pass);
-
-      var user = FirebaseAuth.instance.currentUser;
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString('username', user!.uid);
-    } on FirebaseAuthException catch (e) {
-      error = e.code;
-    }
-
-    if (error != "") {
-      print('xyz: ' + error);
-      showDialog(
-          context: context,
-          builder: (ontext) => AlertDialog(
-                title: Text("Error"),
-                content: SingleChildScrollView(
-                  child: ListBody(
-                    children: [
-                      Text(error),
-                      Text("Please try again!"),
-                    ],
-                  ),
-                ),
-                actions: [
-                  TextButton(
-                    child: Text("Ok"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ));
-    } else {
-      Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,7 +101,14 @@ class _SigninPageState extends State<SigninPage> {
                 child: ElevatedButton(
                   child: Text("Sign in"),
                   onPressed: () async {
-                    await signin();
+                    await signin(emailcontroller.text, passwordcontroller.text,
+                            context)
+                        .then((value) {
+                      print("here");
+                      if (value == true)
+                        Navigator.of(context)
+                            .pushNamedAndRemoveUntil('/', (route) => false);
+                    });
                     //Navigator.pop(context);
                   },
                 ),
